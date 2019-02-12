cbceed5af2192784b3c7099b5da3742cda56864a
shonenada <shonenada@gmail.com>
2019-02-12 08:56:31 +0800

    feat: add about

--- src/Components/Root/About/index.js
+++ src/Components/Root/About/index.js
@@ -6,11 +6,22 @@ import './index.css';
 import tiechui from './tiechui.png';
 
 class About extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      introduction: '3.5 生小公狗'
+    };
+  }
+
   render() {
     return (
       <div className="About">
         <Header match={this.props.match}/>
         <div className="About-Container">
+          <div className="About-intro">
+            <h1>TIE CHUI</h1>
+            {this.state.introduction}
+          </div>
           <div className="About-tiechui" style={{backgroundImage: `url(${tiechui})`}}></div>
         </div>
       </div>
